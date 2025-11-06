ed246004343fa92b75e85b99a0d790075d43d87f
Sanekakf <sashasaltikov2007@gmail.com>
2025-11-06 20:23:16 +0300

    Added painting requests get request

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import psycopg
 app = Flask(__name__)
 api = Api(app)
 
+
 # Connect db
 
 
-
 class RepairRequests(Resource):
-
 
     def get(self):
         res = {}
@@ -27,36 +26,49 @@ class RepairRequests(Resource):
             print(item)
 
             res[l + 1] = {"id": item[0],
-                               "ownerName": item[1],
-                               "phoneNumber": item[2],
-                               "carModel": item[3],
-                               "issueDescription": item[4],
-                               "date": item[5].strftime("%Y-%m-%d"),
-                               "time": item[6],
-                               "status": item[7],
-                               "completionTime": item[8],
-                               "completionDate": item[9]
-                               }
+                          "ownerName": item[1],
+                          "phoneNumber": item[2],
+                          "carModel": item[3],
+                          "issueDescription": item[4],
+                          "date": item[5].strftime("%Y-%m-%d"),
+                          "time": item[6],
+                          "status": item[7],
+                          "completionTime": item[8],
+                          "completionDate": item[9]
+                          }
         conn.close()
         return res
 
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("ownerName", type=str)
-        parser.add_argument("phoneNumber", type=str)
-        parser.add_argument("carModel", type=str)
-        parser.add_argument("issueDescription", type=str)
-        parser.add_argument("date", type=str)
-        parser.add_argument("time", type=str)
-        parser.add_argument("status", type=str)
-        parser.add_argument("completionTime", type=str)
-        parser.add_argument("completionDate", type=str)
-        bigbob = parser.parse_args()
-        print(bigbob)
-        return bigbob
+class PaintingRequests(Resource):
+
+    def get(self):
+        res = {}
+        conn = psycopg.connect(dbname="requests",
+                               user="me1",
+                               password="99@leX216",
+                               host="89.223.68.159",
+                               port="5432"
+                               )
+        c = conn.cursor()
+        c.execute("select * from painting_requests")
+        for item in c.fetchall():
+            l = len(res)
+            print(item)
+
+            res[l + 1] = {"id": item[0],
+                          "ownerName": item[1],
+                          "phoneNumber": item[2],
+                          "carModel": item[3],
+                          "color": item[4],
+                          "date": item[5].strftime("%Y-%m-%d"),
+                          "status": item[6],
+                          }
+        conn.close()
+        return res
 
 
 api.add_resource(RepairRequests, '/api/repair_requests')
+api.add_resource(PaintingRequests, '/api/painting_requests')
 api.init_app(app)
 
 
